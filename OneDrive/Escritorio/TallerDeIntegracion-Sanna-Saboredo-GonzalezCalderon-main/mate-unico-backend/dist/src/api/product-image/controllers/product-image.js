37caeadbd6c77801c44e1831b214db81627724ab
Lucasaboredo <lucandresvelzi@gmail.com>
2025-12-08 15:35:00 -0300

    final

---
+++ OneDrive/Escritorio/TallerDeIntegracion-Sanna-Saboredo-GonzalezCalderon-main/mate-unico-backend/dist/src/api/product-image/controllers/product-image.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * product-image controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::product-image.product-image');
