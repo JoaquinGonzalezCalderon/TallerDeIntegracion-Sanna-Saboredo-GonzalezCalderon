2065878b25574ac8f7718a128f6f69a766c78706
Joaquin Gonzalez Calderon <startgame1975@gmail.com>
2025-12-09 14:50:19 -0300

    piuo

--- mate-unico-backend/types/generated/components.d.ts
+++ mate-unico-backend/types/generated/components.d.ts
@@ -1,32 +1,3 @@
-import type { Schema, Struct } from '@strapi/strapi';
-
-export interface InventarioVariante extends Struct.ComponentSchema {
-  collectionName: 'components_inventario_variante';
-  info: {
-    description: '';
-    displayName: 'Variante';
-  };
-  attributes: {
-    color: Schema.Attribute.String;
-    grabado: Schema.Attribute.Boolean;
-    nombre: Schema.Attribute.String & Schema.Attribute.Required;
-    precio: Schema.Attribute.Decimal & Schema.Attribute.Required;
-    sku: Schema.Attribute.String;
-    stock: Schema.Attribute.Integer &
-      Schema.Attribute.Required &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 0;
-        },
-        number
-      >;
-  };
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'inventario.variante': InventarioVariante;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
